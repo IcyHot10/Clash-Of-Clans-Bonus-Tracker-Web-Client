85df3db345a8301de1da1268243fa3591583589a
Abdul Gany Osman <abdulganyosman@gmail.com>
2025-05-08 11:27:06 +0200

    Added error screens and input verification

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,9 +5,10 @@ import { LeaderboardComponent } from "./leaderboard/leaderboard.component";
 import { MissingdataalertComponent } from "./missingdataalert/missingdataalert.component";
 import { HttpClient } from '@angular/common/http';
 import { NgxSpinnerModule } from 'ngx-spinner';
+import { environment } from '../environments/environment';
 
 interface Clan {
-  state: 'LOADING' | 'SUCCESS' | 'ERROR' | 'NON-CWL' | 'UNSELECTED';
+  state: 'LOADING' | 'SUCCESS' | 'ERROR' | 'NON-CWL' | 'UNSELECTED' | 'NO-CLAN' | 'RATE-LIMITED';
   clanName?: string;
   clanLogo?: string;
   leaderboard?: any[];
@@ -25,15 +26,24 @@ export class AppComponent {
   clan: Clan = { state: 'UNSELECTED' };
   selected = false;
   private http = inject(HttpClient)
-  base_url = 'http://127.0.0.1:5000/leaderboard/'
+  base_url = environment.BASE_URL;
+
 
   onClanTagSelected(clanTag: string) {
-    console.log(clanTag);
-    this.clan = {'state': 'LOADING'}
-    this.selected = true;
-    this.http.get<Clan>(this.base_url+clanTag.replace('#','%23')).subscribe(config => {
-      console.log(config);
-      this.clan = config;
-    })
+    if (this.clan.state != 'LOADING'){
+      if (clanTag == ""){
+        this.clan.state = 'UNSELECTED'
+      }
+      else {
+        console.log(clanTag);
+        this.clan = {'state': 'LOADING'};
+        this.selected = true;
+        this.http.get<Clan>(this.base_url+clanTag.replace('#','%23')).subscribe(config => {
+          console.log(config);
+          this.clan = config;
+        })
+      }
+    }
+
   }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,16 +1,31 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
   @Output() ClanTagSelected = new EventEmitter<string>();
+  clanTag: string = '';
 
   onClanTagSelected(clanTag: string) {
     this.ClanTagSelected.emit(clanTag);
+  }
+
+  onInput(event: Event) {
+    const input = event.target as HTMLInputElement;
+    let value = input.value;
+
+    if (!value.startsWith('#')) {
+      value = '#' + value;
+    }
+    value = value.toUpperCase().replace(/[^#A-Z0-9]/g, '');
+
+    // Update the input value
+    this.clanTag = value;
   }
 }
